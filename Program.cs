bcac17056ccd304801d7fb88b88072380ecf61c5
jules <jules.white1@protonmail.com>
2025-07-11 02:27:02 +0100

    add customers csv and add support for appending lines

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿string STAFF_CSV = "staff.csv";
+﻿#pragma warning disable CS8321
+#pragma warning disable CS0219
+
+string STAFF_CSV = "staff.csv";
+string CUSTOMERS_CSV = "customers.csv";
+bool LOGGED_IN = true;
 
 Main();
 
 void Main() {
-    Login();
     MainMenu();
 }
 
@@ -11,11 +15,12 @@ void Login() {
     string[,] staff_details = ReadCsv(STAFF_CSV);
 
     Console.Clear();
-    string username = Prompt("Please Enter Your Username");
-    string password = Prompt("Please Enter Your Password");
+    ReadOnlySpan<char> username = Prompt("Please Enter Your Username");
+    ReadOnlySpan<char> password = Prompt("Please Enter Your Password");
 
     for(int i = 0; i < staff_details.GetLength(0); i++) {
         if(staff_details[i, 0] == username && staff_details[i, 1] == password) {
+            LOGGED_IN = true;
             return;
         }
     }
@@ -27,8 +32,9 @@ void Login() {
 }
 
 void MainMenu() {
-    Console.Clear();
-    Banner("Main Menu");
+    int minimum_option_index = LOGGED_IN ? 1 : 0;
+    int selected_option_index = minimum_option_index;
+    string[] menu_options = { "Login", "Record Customer Details", "Take Quiz", "Lap Times", "Report" };
 
     // Login
     // Options ( Logged In )
@@ -37,7 +43,51 @@ void MainMenu() {
     // Reaction Test ( Logged In )
     // Allowed Category ( Logged In )
 
-    while(true) {}
+    while(true) {
+        if(LOGGED_IN) minimum_option_index = 1;
+
+        Console.Clear();
+        Banner("Main Menu");
+        Console.WriteLine();
+
+        if(selected_option_index < minimum_option_index) selected_option_index = menu_options.Length - 1;
+        if(selected_option_index >= menu_options.Length) selected_option_index = 0;
+
+        for(int i = minimum_option_index; i < menu_options.Length; i++) {
+            if(!LOGGED_IN && i != 0) ColourGray();
+            Console.WriteLine($"{(selected_option_index == i ? "> " : "")}{menu_options[i]}");
+            ColourWhite();
+        }
+
+        ConsoleKeyInfo key = Console.ReadKey();
+        if(key.Key == ConsoleKey.J) selected_option_index++;
+        if(key.Key == ConsoleKey.K) selected_option_index--;
+
+        if(key.Key == ConsoleKey.L) {
+            ShowMenu((MenuOption)selected_option_index);
+        }
+    }
+}
+
+void ShowMenu(MenuOption option) {
+    switch(option) {
+        case MenuOption.LOGIN:
+            Login();
+            break;
+        case MenuOption.RECORD_DETAILS:
+            if(!LOGGED_IN) break;
+            NewCustomer();
+            break;
+        case MenuOption.QUIZ:
+            if(!LOGGED_IN) break;
+            break;
+        case MenuOption.LAP_TIME:
+            if(!LOGGED_IN) break;
+            break;
+        case MenuOption.REPORTS:
+            if(!LOGGED_IN) break;
+            break;
+    }
 }
 
 string[,] ReadCsv(string path) {
@@ -60,10 +110,51 @@ string[,] ReadCsv(string path) {
     return csv_data;
 }
 
-string Prompt(string prompt) {
+void WriteCsv(string path, string[,] data) {
+    File.WriteAllText(path, "");
+    foreach(var line in data) {
+        string joined = string.Join(",", line);
+        File.AppendAllText(path, joined);
+    }
+}
+
+void AppendCsv(string path, string data) {
+    File.AppendAllText(path, data);
+    File.AppendAllText(path, "\n");
+}
+
+void NewCustomer() {
+    Console.Clear();
+    ReadOnlySpan<char> first_name = Prompt("What is the Customers' Forename?");
+    ReadOnlySpan<char> surname = Prompt("What is the Customers' Surname?");
+    ReadOnlySpan<char> DOB = Prompt("What is the Customers' DOB?");
+    ReadOnlySpan<char> gender = Prompt("What is the Customers' Gender?");
+
+    bool valid_date = DateTime.TryParse(DOB, out DateTime _);
+
+    if(!valid_date) {
+        ColourRed();
+        Notify("Invalid Date Was Input.");
+        ColourWhite();
+        NewCustomer();
+        return;
+    }
+
+    // Sanitise Here
+    Notify("Created New Customer.");
+
+    string first_name_sanitised = new string(first_name.ToArray().Where(x => char.IsAsciiLetterOrDigit(x)).ToArray());
+    string surname_sanitised = new string(surname.ToArray().Where(x => char.IsAsciiLetterOrDigit(x)).ToArray());
+    string DOB_sanitised = new string(DOB.ToArray().Where(x => char.IsAsciiLetterOrDigit(x) || x == '/').ToArray());
+    string gender_sanitised = new string(gender.ToArray().Where(x => char.IsAsciiLetterOrDigit(x)).ToArray());
+
+    AppendCsv(CUSTOMERS_CSV, $"{first_name_sanitised},{surname_sanitised},{DOB_sanitised},{gender_sanitised}");
+}
+
+ReadOnlySpan<char> Prompt(string prompt) {
     Console.WriteLine($"{prompt}");
     Console.Write($"> ");
-    return Console.ReadLine()!;
+    return Console.ReadLine().AsSpan()!;
 }
 
 void Notify(string message) {
@@ -76,9 +167,19 @@ void ColourRed() => Console.ForegroundColor = ConsoleColor.Red;
 void ColourGreen() => Console.ForegroundColor = ConsoleColor.Green;
 void ColourBlue() => Console.ForegroundColor = ConsoleColor.Blue;
 void ColourWhite() => Console.ForegroundColor = ConsoleColor.White;
+void ColourGray() => Console.ForegroundColor = ConsoleColor.Gray;
 
 void Banner(string message) {
     Console.WriteLine("---------------------");
     Console.WriteLine($"{message}");
     Console.WriteLine("---------------------");
+}
+
+enum MenuOption {
+    LOGIN = 0,
+    RECORD_DETAILS = 1,
+    QUIZ = 2,
+    REACTION_TEST = 3,
+    LAP_TIME = 4,
+    REPORTS = 5
 }
